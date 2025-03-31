b09e50a47e55e6b01e20eac41ffd00a8428061ef
Bertrand Ernst <b.ernst@aegaeon-it.com>
2025-03-31 13:56:04 +0200

    Remove dependency to Angular

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,10 @@
 import type { Config } from 'jest';
-import presets from 'jest-preset-angular/presets';
 
 const config: Config = {
     rootDir: 'src',
-    ...presets.createCjsPreset({tsconfig: '<rootDir>/../tsconfig.jest.json'}),
+    transform: {
+        "^.+\.tsx?$": ["ts-jest",{}],
+    },
     verbose: true,
     testRegex: ['(.+).pact.jest.ts$']
 };
